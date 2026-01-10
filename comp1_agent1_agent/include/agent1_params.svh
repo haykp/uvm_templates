23d4fd7c81d186aa54d85d93dd7f0f0a5615c2f4
haykp <haykpet@gmail.com>
2026-01-10 09:27:10 +0100

    updated
    added params,types in one place

---
+++ comp1_agent1_agent/include/agent1_params.svh
@@ -0,0 +1,6 @@
+`ifndef AGENT1_PARAMS
+`define AGENT1_PARAMS
+
+// paramter some param
+
+`endif
---
+++ comp1_agent1_agent/include/agent1_types.svh
@@ -0,0 +1,7 @@
+`ifndef AGENT1_TYPES
+`define AGENT1_TYPES
+
+//`define some time
+// typeded enum {}
+
+`endif
