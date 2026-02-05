60a5db047f131002d69cfed8cf07a7ec090d7559
haykp <haykpet@gmail.com>
2026-02-05 14:41:43 +0100

    latest

---
+++ comp1_env1_env/scripts/tb_files.f
@@ -0,0 +1,7 @@
+-incdir ${UVM_DIR}/comp1_env1_env/include
+-incdir ${UVM_DIR}/comp1_env1_env/include/seq
+-incdir ${UVM_DIR}/comp1_env1_env/include/tests
+-incdir ${UVM_DIR}/comp1_env1_env/include/utils
+
+${UVM_DIR}/comp1_env1_env/comp1_env1_env_pkg.sv
+${UVM_DIR}/comp1_env1_env/comp1_env1_tb_if.sv
